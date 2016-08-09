2e5839558634a080de174e3d95cae0035dfb81f9
Jukka Villstedt <jukka.villstedt@nitor.fi>
2016-08-09 16:35:04 +0300

    x

--- src/cor/web_socket.clj
+++ src/cor/web_socket.clj
@@ -4,7 +4,10 @@
             [taoensso.sente.server-adapters.http-kit :as http-kit-adapter]
             [ring.middleware.keyword-params :as keyword-params]
             [ring.middleware.params :as params]
-            [clojure.core.async :as async]))
+            [clojure.core.async :as async]
+            [compojure.core :as compojure]
+            [ring.middleware.keyword-params :as keyword-params]
+            [ring.middleware.params :as params]))
 
 (defn user-id [req]
   (:client-id req))
@@ -25,3 +28,14 @@
 (defn broadcast-message [channel-socket messsage-id data]
   (doseq [uid (:any @(:connected-uids channel-socket))]
     ((:send-fn channel-socket) uid [messsage-id data])))
+
+
+(defn routes [path channel-socket]
+  [(compojure/GET  path req ((:ajax-get-or-ws-handshake-fn channel-socket) req))
+   (compojure/POST path req ((:ajax-post-fn channel-socket)                req))])
+
+
+(defn wrap [app]
+  (-> app
+      keyword-params/wrap-keyword-params
+      params/wrap-params))
